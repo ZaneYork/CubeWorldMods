eec5c473f58c9e01b64d9af3ce54a7b08a8a442f
yangzhi <@4F!xZpJwly&KbWq>
2019-10-13 13:28:38 +0800

    First version

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "cwmods/cwmods.h"
+//#include "logger.h"
+//using namespace LOGGER;
+//CLogger* logger;
+
+EXPORT int HandleInventoryCheck(cube::Creature* player, cube::Item *item) {
+    //logger->TraceInfo("Player:%s, Item:%d\n", player->entity_data.name, item->id);
+    // 1: full, 2: not full, 0: origin logic
+    return 2;
+}
+
+//EXPORT void ModInitialize() {
+//    logger = new CLogger(LogLevel_Info,CLogger::GetAppPathA());
+//    logger->ChangeLogLevel(LOGGER::LogLevel_Error);
+//}
