cf44379f9d1d5fca69a82861d73d0e333b574e02
yangzhi <@4F!xZpJwly&KbWq>
2019-10-15 21:33:16 +0800

    Add UnlockMapIconMod

---
+++ UnlockMapIconMod/main.cpp
@@ -0,0 +1,7 @@
+#include "main.h"
+#include "../cwmods/cwmods.h"
+
+EXPORT int HandleCheckMapIconVisibility(cube::Creature* player, int a2, int a3) {
+    // 1: visible, 0: origin logic
+    return 1;
+}
